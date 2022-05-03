a591b6338b60de7140a70265c8977083c4d9576b
xingyuzzzhu <xingyuzzzhu@users.noreply.github.com>
2022-05-03 00:49:03 +0000

    Deploying to production from @ cse112-sp22-group4/Electric-Pomato@0264f44a2cefe3d033141cbfa97aeef93674bf69 🚀

--- index.2d4e6f1c.js
+++
@@ -1,1 +0,0 @@
-class t extends HTMLElement{constructor(){super();const t=JSON.parse(localStorage.getItem("History"));if(t&&t.tasklists.length>0){const t=document.createElement("a");t.classList.add("btn","btn-info","btn-lg"),t.href="./done.html",t.textContent="View Logs",this.appendChild(t)}}}customElements.define("logs-button",t);
