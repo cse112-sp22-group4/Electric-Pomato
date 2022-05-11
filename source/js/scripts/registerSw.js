3b6d63d43018eec77cec445603fb25bf9eaa73ef
Chris Y <chris01wy@gmail.com>
2022-05-10 20:21:46 -0700

    Fixed eslint problem

--- source/js/scripts/registerSw.js
+++ source/js/scripts/registerSw.js
@@ -1,5 +1,6 @@
-  // Service worker
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(new URL('../../serviceWorker.js', import.meta.url), {type: 'module'})
-      .then(() => { console.log('Service Worker Register'); });
-  }
+// Service worker
+if ('serviceWorker' in navigator) {
+  // eslint-disable-next-line no-path-concat
+  navigator.serviceWorker.register(new URL('../../serviceWorker.js', `${window.location.origin}/${__dirname}`), { type: 'module' })
+    .then(() => { console.log('Service Worker Register'); });
+}
