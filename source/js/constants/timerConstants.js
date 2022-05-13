7225a992422b21c6960eead8b0180740dafb19d6
Steven <stevenharris434@gmail.com>
2022-05-13 15:51:30 -0700

    move timer constants to file

---
+++ source/js/constants/timerConstants.js
@@ -0,0 +1,8 @@
+// Defines the default timer duration in minutes for each session type
+const timerConstants = {
+  DEFAULT_WORK_SESSION_DURATION: 25,
+  DEFAULT_SHORT_BREAK_DURATION: 5,
+  DEFAULT_LONG_BREAK_DURATION: 25,
+};
+
+export default timerConstants;
