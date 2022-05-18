2c5ec93217779ce7dde35c37d7d40845659a9361
46chris <chris01wy@gmail.com>
2022-05-18 13:41:06 -0700

    Added Date.now time calculation

--- source/js/classes/Timer.js
+++ source/js/classes/Timer.js
@@ -14,8 +14,8 @@
 class Timer {
   /**
    * Constructs the timer object.
-   * @param {number} minutes - Numerical value of maximum minutes.
-   * @param {number} seconds - Numerical value of maximum seconds.
+   * @param {number} minutes - The number of minutes the timer counts down (max is 99)
+   * @param {number} seconds - The number of seconds the timer counts down (max is 59)
    * @param {callback} callbackEverySecond - callback called every second.
    */
   constructor(minutes, seconds, callbackEverySecond) {
@@ -34,8 +34,7 @@ class Timer {
    */
   startTimer() {
     return new Promise((resolve) => {
-      // Count down immediately
-      // This is where the timer callbacks a function every second
+      // Count down immediately and call the callback (callback is mostly used to set the text)
       const resolveMessage = 'Timer Finished';
       if (this.callbackEverySecond !== null) {
         this.callbackEverySecond(this.minutes, this.seconds);
@@ -46,21 +45,28 @@ class Timer {
       } else if (this.seconds === 0 && this.minutes === 0) {
         resolve(resolveMessage);
       }
+      console.log(Date.now());
       this.seconds -= 1;
+
       // Keep counting down until it reaches 0 mins 0 seconds (inclusive)
+      const end = Math.floor(Date.now() / 1000) + (this.minutes * 60) + (this.seconds);
       const countdown = setInterval(() => {
         // This is where the timer callbacks a function every second
         if (this.callbackEverySecond !== null) {
           this.callbackEverySecond(this.minutes, this.seconds);
         }
-        if (this.seconds === 0 && this.minutes !== 0) {
-          this.minutes -= 1;
-          this.seconds = 60;
-        } else if (this.seconds === 0 && this.minutes === 0) {
+        if (this.seconds === 0 && this.minutes === 0) {
           resolve(resolveMessage);
           clearInterval(countdown);
         }
-        this.seconds -= 1;
+        // Get start and end time in seconds
+        const now = Math.floor(Date.now() / 1000);
+
+        // Difference in seconds
+        const diff = end - now;
+
+        this.minutes = Math.floor(diff / 60);
+        this.seconds = diff % 60;
       }, 1000);
     });
   }
