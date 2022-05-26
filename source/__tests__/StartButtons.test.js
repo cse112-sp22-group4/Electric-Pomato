43838278c9d081caaa4248af8ac075afcefe2532
xingyuzzzhu <x2zhu@ucsd.edu>
2022-05-25 19:27:35 -0700

    remove button container

--- source/__tests__/StartButtons.test.js
+++ source/__tests__/StartButtons.test.js
@@ -6,8 +6,8 @@ describe('StartButtons tests', () => {
     const startButtons = new StartButtons();
     // start buttons generate one child start container
     expect(startButtons.childElementCount).toBe(1);
-    expect(startButtons.firstChild.nodeName).toBe('DIV');
-    expect(startButtons.firstChild.childElementCount).toBe(1);
+    expect(startButtons.firstChild.nodeName).toBe('BUTTON');
+    // expect(startButtons.firstChild.childElementCount).toBe(1);
   });
 
   test('Constructor When TaskList is not empty', () => {
@@ -23,7 +23,7 @@ describe('StartButtons tests', () => {
     expect(startButtons.childElementCount).toBe(2);
 
     // right child button has another child element
-    expect(startButtons.lastChild.childElementCount).toBe(1);
+    // expect(startButtons.lastChild.childElementCount).toBe(1);
   });
 
   test('Clicking Resume Session when session started', () => {
@@ -31,22 +31,20 @@ describe('StartButtons tests', () => {
     const taskList = new TaskList();
     taskList.createTask('first task', 1);
     taskList.save();
- 
     localStorage.setItem('Timer', false);
-    localStorage.setItem('CurrentPomos', 2); 
+    localStorage.setItem('CurrentPomos', 2);
     localStorage.setItem('Started', true);
 
     // create buttons
     const startButtons = new StartButtons();
 
     // Click on resume session anchor
-    startButtons.lastChild.firstChild.click(); 
+    startButtons.lastChild.click();
 
     // Timer state reset to Pomodoro
     expect(localStorage.getItem('Timer')).toEqual('true');
 
     // CurrentPomos reset to 0
     expect(Number(localStorage.getItem('CurrentPomos'))).toBe(0);
-
   });
 });
