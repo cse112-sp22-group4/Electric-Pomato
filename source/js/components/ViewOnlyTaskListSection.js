6bbf84234bbf9c3216cb474b9e68914eee982ffc
Luke <lmenezes@ucsd.edu>
2022-06-06 23:24:54 -0700

    edited view only max task length

--- source/js/components/ViewOnlyTaskListSection.js
+++ source/js/components/ViewOnlyTaskListSection.js
@@ -23,8 +23,8 @@ class ViewOnlyTaskListSection extends HTMLElement {
     this.insertTitle(title);
     this.insertHeaderRow();
     data.forEach(({ name, expected, actual }, i) => {
-      if (name.length > 12) {
-        name = `${name.substring(0, 12)}...`;
+      if (name.length > 15) {
+        name = `${name.substring(0, 15)}...`;
       }
       this.insertRow(i + 1, name, expected, actual, title);
     });
